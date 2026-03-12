c05506f4304ab9fb9cd2f1d967152546f23e8a88
MiggRabbid <mig26@inbox.ru>
2026-03-12 12:11:10 +0300

    Fix: Header

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,16 +1,13 @@
 // Библиотеки
-import { Box, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 
 function Header() {
   return (
-    <Box
-      component="header"
-      className="mx-3! my-3! h-fit rounded-lg bg-white px-6 py-2! shadow-md"
-    >
+    <header className="mx-3! my-3! h-fit rounded-lg bg-white px-6 py-2! shadow-md">
       <Typography component="h1" variant="h4" align="center">
         {'Тестовое задание для "Сириус Курсы"'}
       </Typography>
-    </Box>
+    </header>
   );
 }
 
